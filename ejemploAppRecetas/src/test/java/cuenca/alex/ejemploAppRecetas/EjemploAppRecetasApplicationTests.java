dbbafa3402f87545f43039fa942d2c1d1f636791
Administrador <Administrador@LEGION-Y545>
2020-07-14 16:17:21 -0500

    Update1

---
+++ ejemploAppRecetas/src/test/java/cuenca/alex/ejemploAppRecetas/EjemploAppRecetasApplicationTests.java
@@ -0,0 +1,13 @@
+package cuenca.alex.ejemploAppRecetas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjemploAppRecetasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
